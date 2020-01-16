761f8ad166e17b9bb18b132296fa5ca46a2cd8b2
KHJ <akdlakdrn>
2020-01-16 18:29:30 +0900

    롬북_테스트 코드등록

--- src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)//1
 @WebMvcTest//2
@@ -20,11 +22,27 @@ public class HelloControllerTest {
     private MockMvc mvc;//4
 
     @Test
-    public void hello가_리턴된다() throws Exception{
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("hello"))//5
-                .andExpect(status().isOk())//6
-                .andExpect(content().string(hello));//7
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
+
+    @Test
+    public void 롬북_기능_테스트(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
 }
