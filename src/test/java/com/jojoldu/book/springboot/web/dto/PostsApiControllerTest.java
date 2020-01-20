0213bb81226a31a88e2ebbf3a32e9b8a93244023
KHJ <akdlakdrn>
2020-01-20 15:48:33 +0900

    PostApi 등록 수정 조회 test

--- src/test/java/com/jojoldu/book/springboot/web/dto/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/PostsApiControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -68,6 +70,41 @@ public class PostsApiControllerTest {
         String test2 = all.get(0).getTitle();
         System.out.println(id);
         System.out.println(test2);
+
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception{
+        //given
+        Posts savedPosts = postsRepository.save(
+                Posts.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                        .build());
+
+        Long updateId = savedPosts.getId();
+        String exceptedTitle = "title2";
+        String exceptedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(exceptedTitle)
+                .contet(exceptedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "api/v1/posts/"+updateId;
+        HttpEntity<Long> resquestEntity = new HttpEntity(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url,HttpMethod.PUT,resquestEntity,Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(exceptedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(exceptedContent);
 
     }
 }
